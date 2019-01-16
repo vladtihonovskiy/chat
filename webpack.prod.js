7e53f86ef89137be7b74af4eb6e0c3c5c57ba8a3
Victor <flarevictor@gmail.com>
2019-01-16 13:07:07 +0200

    herroku fix

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,6 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
-
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 	devtool: "devtool: 'source-map'",
