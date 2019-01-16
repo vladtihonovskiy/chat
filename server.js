ebf7dc5df1083afff529c2c977913753b1e45544
Victor <flarevictor@gmail.com>
2019-01-16 12:58:07 +0200

    herroku fix

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.resolve(__dirname, 'build')));
 
 app.get('*', function(req, res) {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+	res.sendFile(path.resolve(__dirname, 'build', "index.html"));
 });
 
 app.listen(process.env.PORT || 5000);
